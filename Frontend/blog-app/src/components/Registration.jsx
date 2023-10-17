fb079cfe7ddb8126320c4c628c3479eb81c69907
omp12345 <opsgp1999@gmail.com>
2023-10-16 23:43:52 -0700

    ok

--- Frontend/blog-app/src/components/Registration.jsx
+++ Frontend/blog-app/src/components/Registration.jsx
@@ -10,109 +10,114 @@ function Registration() {
     email: '',
     password: '',
   });
-  const navigate=useNavigate()
+  const navigate = useNavigate();
 
-  const [alrt,setalrt]=useState("")
-  const [msg,setMsg]=useState("")
+  const [alrt, setAlrt] = useState("");
+  const [msg, setMsg] = useState("");
+  const [blankFieldMsg, setBlankFieldMsg] = useState(""); // Message for blank fields
+  const [invalidEmailMsg, setInvalidEmailMsg] = useState(""); // Message for invalid email format
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+
+    // Clear the messages
+    setBlankFieldMsg("");
+    setInvalidEmailMsg("");
   };
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
-  
-    // Check if the form data is empty
+
+    // Check if any of the form fields are empty
     if (!formData.username || !formData.email || !formData.password) {
-      // Navigate to the error page
-      alert("Please fill the input")
+      setBlankFieldMsg("Please fill in all fields");
+    } else if (!isEmailValid(formData.email)) {
+      setInvalidEmailMsg("Invalid email format");
     } else {
-     
+      // Clear the messages
+      setBlankFieldMsg("");
+      setInvalidEmailMsg("");
+
       axios.post("https://good-jade-shrimp-wrap.cyclic.app/api/register", formData)
         .then((res) => {
           console.log(res.data);
-          setalrt(res.data.message);
+          setAlrt(res.data.message);
           speakNotification(res.data.message);
-          navigate("/login")
+          navigate("/login");
         })
         .catch((error) => {
           console.error("Registration failed:", error);
-          
-         navigate("/error");
+          navigate("/error");
         });
     }
   };
-  
-  
-  
-  
-  
-  
 
   const speakNotification = (message) => {
     const synth = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance(message);
     synth.speak(utterance);
-    alert(`${alrt}`)
-
-
-
-
-
-    
-    
-
-  
   };
-  const handleclick=()=>{
-navigate("/login")
+
+  const handleclick = () => {
+    navigate("/login");
   }
+  
+  const isEmailValid = (email) => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zAZ0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  };
+ 
+
 
   return (
     <div className='main'>
-    <div className="registration-container">
-      <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            placeholder='username'
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            placeholder='email'
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            placeholder='password'
-          />
-        </div>
-        <button type="submit">Register</button>
-        <button onClick={handleclick} style={{"color":'teal'}} type="submit" className="login-button">Login</button>
+      <div className="registration-container">
+        <h2>Register</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+              placeholder='username'
 
-      
-      </form>
-      
-    </div>
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder='email'
+             
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder='password'
+            />
+          </div>
+          {blankFieldMsg && <p style={{ color: 'smoke' }}>{blankFieldMsg}</p>}
+          {invalidEmailMsg && <p style={{ color: 'red' }}>{invalidEmailMsg}</p>}
+          {alrt && <p>{alrt}</p>}
+          <button type="submit">Register</button>
+          <button onClick={handleclick} style={{"color": 'teal'}} type="submit" className="login-button">Login</button>
+        </form>
+      </div>
     </div>
   );
 }
